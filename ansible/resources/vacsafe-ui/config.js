91f7783d52db1fd8ee86aed8d2a0b72c70d523e0
jbride <jbride2001@yahoo.com>
2021-10-12 02:53:37 -0600

    initial end-to-end test now working via ansible in OCP 4.8

---
+++ ansible/resources/vacsafe-ui/config.js
@@ -0,0 +1,5 @@
+// Get these values from Onboarding Documentation
+window.REACT_APP_PAM_URL_BASE = "https://{{ vacsafe_service_hostname }}"
+window.REACT_APP_PAM_USER = "pamAdmin"
+window.REACT_APP_PAM_PASSWORD = "pam"
+window.REACT_APP_PAM_CONTAINER_ID="{{ vacsafe_service_deployment_name }}"
